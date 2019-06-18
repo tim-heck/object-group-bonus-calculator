11952cfb041d75c44589db14923121728f62288c
Tim Heck <tih99924@bethel.edu>
2019-06-18 16:07:04 -0500

    completed bonus calculator

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
-const employees = [
-  {
+$(document).ready(readyNow);
+
+const employees = [{
     name: 'Atticus',
     employeeNumber: '2405',
     annualSalary: '47000',
@@ -40,4 +41,66 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+function displayEmployees() {
+  let el = $('#output');
+  el.empty();
+  for (let item of employees) {
+    let employeeBonus = employeeCalculator(item);
+    el.append('<ul>');
+    el.append('<li>' + 'Name: ' + employeeBonus.name + '</li>');
+    el.append('<li>' + ' Bonus Percentage: ' + employeeBonus.bonusPercentage  + '</li>');
+    el.append('<li>' + ' Total Compensation: ' + employeeBonus.totalCompensation + '</li>');
+    el.append('<li>' + ' Total Bonus: ' + employeeBonus.totalBonus + '</li>');
+    el.append('</ul>');
+  }
+}
+
+function employeeCalculator(object) {
+  let bonusPercentage = 0;
+  let totalCompensation = 0;
+  let totalBonus = 0;
+
+  let reviewRating = object.reviewRating;
+
+  if (reviewRating <= 2) {
+    bonusPercentage = 0;
+  } else if (reviewRating === 3) {
+    bonusPercentage = .04;
+  } else if (reviewRating === 4) {
+    bonusPercentage = .06;
+  } else {
+    bonusPercentage = .10;
+  }
+
+  // object.employeeNumber.length ???
+  if ((object.employeeNumber).length === 4) {
+    bonusPercentage += .05;
+  }
+  if (object.annualSalary > 65000) {
+    bonusPercentage -= .01;
+  }
+  if (bonusPercentage > .13) {
+    bonusPercentage = .13;
+  } else if (bonusPercentage < 0) {
+    bonusPercentage = 0;
+  }
+
+  totalBonus = Math.round(object.annualSalary * bonusPercentage);
+
+  totalCompensation = Number(object.annualSalary) + totalBonus;
+
+  let newObject = {
+    name: object.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: totalCompensation,
+    totalBonus: totalBonus
+  }
+
+  return newObject;
+}
+
+function readyNow() {
+  $('#execute').on('click', displayEmployees);
+}
+
+console.log(employees);
